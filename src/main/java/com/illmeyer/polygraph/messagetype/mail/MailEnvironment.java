633dfd66e541713e3cac4018a18570086ac18879
Wolfgang Illmeyer <wolfgang@illmeyer.com>
2013-07-02 23:11:15 +0200

    remove deprecated constants & methods

--- src/main/java/com/illmeyer/polygraph/messagetype/mail/MailEnvironment.java
+++ src/main/java/com/illmeyer/polygraph/messagetype/mail/MailEnvironment.java
@@ -19,64 +19,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.illmeyer.polygraph.messagetype.mail;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
 import java.util.UUID;
 
-import com.illmeyer.polygraph.messagetype.mail.model.Body;
-
-import freemarker.core.Environment;
-import freemarker.template.TemplateModelException;
-import freemarker.template.TemplateScalarModel;
-
 public class MailEnvironment {
-
-	public static void registerParamStringValue(Map<String,Object> src, String name, Map<String,String> dest) throws TemplateModelException {
-		Object o = src.get(name);
-		if (o instanceof TemplateScalarModel) {
-			String s = ((TemplateScalarModel)o).getAsString();
-			if (s!=null && !s.trim().isEmpty())
-				dest.put(name, s);
-		}
-	}
-	
-	public static Stack<String> getTagStack(Environment env) {
-		Object oTagStack = env.getCustomAttribute(MailConstants.ECA_TAGSTACK);
-		if (oTagStack != null && oTagStack instanceof Stack) {
-			@SuppressWarnings("unchecked")
-			Stack<String> result = (Stack<String>)oTagStack; 
-			return result;
-		}
-		Stack<String> result = new Stack<>();
-		env.setCustomAttribute(MailConstants.ECA_TAGSTACK, result);
-		return result;
-	}
-	
-	public static Stack<Body> getPartStack(Environment env) {
-		Object oPartStack = env.getCustomAttribute(MailConstants.ECA_PART_STACK);
-		if (oPartStack != null && oPartStack instanceof Stack) {
-			@SuppressWarnings("unchecked")
-			Stack<Body> result = (Stack<Body>)oPartStack; 
-			return result;
-		}
-		Stack<Body> result = new Stack<>();
-		env.setCustomAttribute(MailConstants.ECA_PART_STACK, result);
-		return result;
-		
-	}
-	
-	public static Map<String,String> getCidMap(Environment env) {
-		Object oCidMap = env.getCustomAttribute(MailConstants.ECA_CIDMAP);
-		if (oCidMap != null && oCidMap instanceof Map) {
-			@SuppressWarnings("unchecked")
-			Map<String,String> result = (Map<String,String>)oCidMap;
-			return result;
-		}
-		Map<String,String> result = new HashMap<>();
-		env.setCustomAttribute(MailConstants.ECA_CIDMAP, result);
-		return result;
-	}
 
 	public static String createContentId() {
 		return UUID.randomUUID().toString().replace("-","").replace("{", "").replace("}", "");
